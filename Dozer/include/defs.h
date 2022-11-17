d303677c79bbc8cf67e64bfa69d30c228943c7b7
1998-felix <felix.gateru@gmail.com>
2022-11-17 11:05:04 +0300

    changes

--- Dozer/include/defs.h
+++ Dozer/include/defs.h
@@ -8,14 +8,14 @@
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //three columns
 char keys[ROWS][COLS] = {
-    {'1','2','3', '4'},
-    {'4','5','6', '4'},
-    {'7','8','9', '4'},
-    {'*','0','#', '4'}
+    {'D','#','0', '*'},
+    {'C','9','8', '7'},
+    {'B','6','5', '4'},
+    {'A','3','2', '1'}
 }; 
 
-byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {8, 7, 6}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {2, 4, 16, 17}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5, 18, 19,23}; //connect to the column pinouts of the keypad
 
 
 
--- Dozer/src/main.cpp
+++ Dozer/src/main.cpp
@@ -5,97 +5,138 @@
 #include "AccelStepper.h"
 
 // define
-#define DRV_M0 32
-#define DRV_M1 31
-#define DRV_EN 34
-#define DRV_RESET 30
-#define DRV_SLEEP 29
-#define DRV_STEP 28
-#define DRV_DIR 27
+#define DRV_M0 12
+#define DRV_M1 14
+#define DRV_EN 13
+#define DRV_RESET 27
+#define DRV_SLEEP 26
+#define DRV_STEP 25
+#define DRV_DIR 33
 #define motorInterfaceType 1
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
-AccelStepper stepper = AccelStepper(motorInterfaceType, DRV_STEP, DRV_DIR);
+AccelStepper myStepper = AccelStepper(motorInterfaceType, DRV_STEP, DRV_DIR);
 
-////
-byte ledPin = 2;
-
+void keypadEvent(KeypadEvent key);
 boolean blink = false;
 boolean ledPin_state;
 const int stepsPerRevolution = 200;
+String inputString;
+int inputValue;
+int dispenseAmount = 0;
+String outPut;
+int currentPosition;
+
+
 
 void setup()
 {
     Serial.begin(115200);
     lcd.init();
-    lcd.setCursor(4,1);
+    lcd.init();
+    lcd.setCursor(4, 1);
     lcd.print("Dozer V 0.0.1");
-    pinMode(LED_BUILTIN, OUTPUT);    // Sets the digital pin as output.
-    digitalWrite(LED_BUILTIN, HIGH); // Turn the LED on.
-    // ledPin_state = digitalRead(ledPin);   // Store initial LED state. HIGH when LED is on.
-    // keypad.addEventListener(keypadEvent); // Add an event listener for this keypad
+    // Store initial LED state. HIGH when LED is on.
+    keypad.addEventListener(keypadEvent); // Add an event listener for this keypad
     pinMode(DRV_M0, OUTPUT);
     pinMode(DRV_M1, OUTPUT);
     pinMode(DRV_EN, OUTPUT);
     pinMode(DRV_RESET, OUTPUT);
     pinMode(DRV_SLEEP, OUTPUT);
-    digitalWrite(DRV_M0, HIGH);
-    digitalWrite(DRV_M1, HIGH);
+    digitalWrite(DRV_M0, LOW);
+    digitalWrite(DRV_M1, LOW);
     digitalWrite(DRV_EN, LOW);
     digitalWrite(DRV_RESET, HIGH);
     digitalWrite(DRV_SLEEP, HIGH);
     delay(1000);
+    pinMode(LED_BUILTIN, OUTPUT);
     lcd.clear();
+    myStepper.setMaxSpeed(1000);
+	myStepper.setAcceleration(800);
+	myStepper.setSpeed(600);
+	
 }
 
 void loop()
 {
-        char key = keypad.getKey();
+    char key = keypad.getKey();
+    
 
-        if (key) {
-            Serial.println(key);
-        }
-        if (blink){
-            digitalWrite(ledPin,!digitalRead(ledPin));    // Change the ledPin from Hi2Lo or Lo2Hi.
-            delay(100);
-        }
+    if (key)
+    {
+        lcd.clear();
+        Serial.println(key);
+        outPut = outPut + key;
+        inputString = inputString + key;
+        inputValue = inputString.toInt();
+        lcd.print(outPut);
+        
+        
     }
 
-    // Taking care of some special events.
-    void keypadEvent(KeypadEvent key){
-        switch (keypad.getState()){
-        case PRESSED:
-            if (key == '#') {
-                Serial.print("Hello World!");     // Remember LED state, lit or unlit.
+}
+
+// Taking care of some special events.
+void keypadEvent(KeypadEvent key)
+{
+    switch (keypad.getState())
+    {
+    case PRESSED:
+        if (key == 'D')
+        {
+            dispenseAmount = dispenseAmount + (outPut.toInt()*200);
+            Serial.print(dispenseAmount);
+            myStepper.moveTo(dispenseAmount);
+            while( myStepper.distanceToGo() != 0){
+                myStepper.run();
             }
-            break;
+        
+            
+            lcd.clear();
+            lcd.print("Dispensing");
+            lcd.print("  ");
+            lcd.print(outPut);
+            delay(1000);
+            lcd.clear();
+            lcd.print("Done dipensing");
+            outPut="";
+            
+
+            lcd.clear();
+        }
+        else if (key == 'C')
+        {
+            outPut="";
+        }
+        break;
 
         case RELEASED:
-            if (key == '*') {
-                digitalWrite(LED_BUILTIN,ledPin_state);    // Restore LED state from before it started blinking.
-                blink = false;
+            if (key == 'D')
+            {
+                outPut="";
             }
             break;
 
-        case HOLD:
-            if (key == '*') {
-                blink = true;    // Blink the LED when holding the * key.
-            }
-            break;
-        }
-    // Set the current position to 0:
-    // stepper.setCurrentPosition(0);
-
-    // // Run the motor forward at 200 steps/second until the motor reaches 400 steps (2 revolutions):
-    // while (stepper.currentPosition() != 400)
-    // {
-    //     stepper.setSpeed(200);
-    //     stepper.runSpeed();
-    // }
-
-    // delay(1000);
-
-    // // Reset the position to 0:
-    // stepper.setCurrentPosition(0);
+        // case HOLD:
+        //     if (key == '*')
+        //     {
+        //         Serial.println("I am held"); // Blink the LED when holding the * key.
+        //     }
+        //     break;
+    }
 }
+// // Set the current position to 0:
+// stepper.setCurrentPosition(0);
+
+// // Run the motor forward at 200 steps/second until the motor reaches 400 steps (2 revolutions):
+// while (stepper.currentPosition() != 400)
+// {
+//     stepper.setSpeed(200);
+//     stepper.runSpeed();
+// }
+
+// delay(1000);
+
+// // Reset the position to 0:
+// stepper.setCurrentPosition(0);
